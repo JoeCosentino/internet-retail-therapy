f81af9a025c0d5b20d819554e6440329fd1d1d1b
Joey Cos <joeycosentino24@gmail.com>
2022-05-22 21:08:49 -0400

    completed get routes for tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,13 +6,16 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   Tag.findAll({
-    where: {
-      tag_name: req.body.tag_name
-    },
+    order: [['tag_name']],
+    attributes: [
+      'id', 'tag_name'
+    ],
     include: [
       {
         model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        through: ProductTag,
+        as: 'tagged_products'
       }
     ]
   })
@@ -26,6 +29,33 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id', 'tag_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        through: ProductTag,
+        as: 'tagged_products'
+      }
+    ]
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json(dbTagData);
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
   // be sure to include its associated Product data
 });
 
